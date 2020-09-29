6faadfd898a68e8d7184930711b23eb65294a360
cdl <1262724276@qq.com>
2020-09-29 23:10:32 +0800

    代码优化

--- src/components/videoPlayer/my-video-2.vue
+++ src/components/videoPlayer/my-video-2.vue
@@ -14,33 +14,38 @@
         />
         <van-loading color="#fff" size="58px" v-show="state.isLoading" />
       </div>
-      <div class="control-bar">
+      <div
+        class="control-bar"
+        :style="{ fontSize: state.fullScreen ? '15px' : '13px' }"
+      >
         <span id="current-time" class="time">{{
           video.currentTime | formatTime
         }}</span>
-        <van-slider
+        <div
           class="progress-bar"
-          v-model="value"
-          button-size="13px"
-          @input="currentTimeChange"
-          @change="moveProgress"
-          @drag-start="touchstart"
-          @drag-end="touchend"
-        />
+          :style="{ width: state.fullScreen ? '80%' : '65%' }"
+        >
+          <div class="buffer" :style="{ width: video.buffer + '%' }"></div>
+          <van-slider
+            v-model="value"
+            class="play-progress-bar"
+            button-size="13px"
+            @input="currentTimeChange"
+            @change="moveProgress"
+            @drag-start="touchstart"
+            @drag-end="touchend"
+          />
+        </div>
         <span id="total-time" class="time">{{
           video.totalTime | formatTime
         }}</span>
         <div class="full-screen" @click="fullscreen">
           <svg-icon
-            v-if="video.fullScreen"
-            style="font-size: 18px;color: white"
-            icon-class="exitFullscreen"
+            v-if="state.fullScreen"
+            class="fullscreen-btn"
+            icon-class="exitfull"
           />
-          <svg-icon
-            v-else
-            style="font-size: 18px;color: white"
-            icon-class="fullscreen"
-          />
+          <svg-icon v-else class="fullscreen-btn" icon-class="fullscreen" />
         </div>
       </div>
     </div>
@@ -55,6 +60,7 @@
       @canplaythrough="state.isLoading = false"
       @waiting="state.isLoading = true"
       @playing="playing"
+      @progress="progress"
     />
   </div>
 </template>
@@ -68,7 +74,8 @@ export default {
       // video控制显示设置
       video: {
         currentTime: "00:00", // 进度时间
-        totalTime: "00:00" // 总时间
+        totalTime: "00:00", // 总时间
+        buffer: 0
       },
       imgSrc: {
         pauseImg: require("assets/video/content_btn_pause.svg"),
@@ -83,7 +90,7 @@ export default {
         fullScreen: false,
         playing: false,
         isLoading: false,
-        isEnd: false,
+        isEnd: false
       }
     };
   },
@@ -106,6 +113,12 @@ export default {
     }
   },
   methods: {
+    progress() {
+      setTimeout(() => {
+        const end = this.$refs.video.buffered.end(0);
+        this.video.buffer = ((end / this.$refs.video.duration) * 100) | 0;
+      }, 100);
+    },
     playing() {
       this.state.isLoading = false;
       this.state.playing = true;
@@ -125,7 +138,7 @@ export default {
       this.$refs.video.play();
     },
     currentTimeChange(value) {
-      this.video.currentTime = value / (100 / this.$refs.video.duration)
+      this.video.currentTime = value / (100 / this.$refs.video.duration);
     },
     moveProgress() {
       this.$refs.video.currentTime =
@@ -133,16 +146,16 @@ export default {
     },
     fullscreen() {
       if (!this.state.fullScreen) {
-        this.state.fullScreen = true;
         window.plus.screen.lockOrientation("landscape-primary");
         setTimeout(() => {
           this.$refs.player.requestFullscreen();
+          this.state.fullScreen = true;
         }, 100);
       } else {
-        this.state.fullScreen = false;
         window.plus.screen.lockOrientation("portrait-primary");
         setTimeout(() => {
           document.exitFullscreen();
+          this.state.fullScreen = false;
         }, 100);
       }
     },
@@ -186,23 +199,45 @@ export default {
 .control-bar {
   width: 90%;
   position: absolute;
-  bottom: 8px;
+  bottom: 4%;
   left: 50%;
   transform: translate(-50%);
-  height: 26px;
+  height: 10%;
   display: flex;
   align-items: center;
   justify-content: space-around;
 }
-.progress-bar {
-  width: 65%;
-}
 .time {
   color: white;
-  font-size: 13px;
+  font-size: 1em;
+}
+.progress-bar {
+  margin-left: 5px;
 }
 .full-screen {
   position: relative;
   top: 1px;
+}
+.fullscreen-btn {
+  font-size: 1.38em;
+  color: white;
+  /*font-size: 18px*/
+}
+.buffer {
+  background-color: red;
+  height: 2px;
+  z-index: 5;
+  position: relative;
+  top: 1px;
+}
+.play-progress-bar {
+  position: relative;
+  bottom: 1px;
+}
+/deep/ .van-slider__button-wrapper {
+  z-index: 10;
+}
+/deep/ .van-slider__bar {
+  z-index: 10;
 }
 </style>
